5f53e3ec66ab4d895cfcf699cb126f58d6ab7356
Filipe Ritto <filipe.ritto@gmail.com>
2021-05-25 20:25:44 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//-== Header fixed on top animation
 (function () {
 
     var headerScroll = getId('header-main'),
@@ -34,44 +35,6 @@
 
 })();
 //------------------------ Header
-
-//-== Header fixed on top animation
-(function () {
-
-    var headerScroll = getId('form-sticker'),
-        scrollHeight = 10,
-        classHeader = 'active';
-
-    //SCROLL
-    window.addEventListener("scroll", scrollOn);
-
-    function scrollOn() {
-        animatedScroll(headerScroll, classHeader, scrollHeight);
-    }
-
-    //Função que on scroll muda o comportamento do elemento
-    function animatedScroll(element, classN, height) {
-        y = pageYOffset; //resgata do objeto window o valor pageYOffset e guarda na variável
-        if (y > height) {
-            element.className = classN;
-        } else {
-            element.className = '';
-        }
-    }
-
-    // Função toggle adiciona ou tira a class do elemento
-    function toggle(element, classe) {
-        element.className = element.className ? '' : classe;
-    }
-
-    //Função que retorna o id do elemento
-    function getId(id) {
-        return document.getElementById(id);
-    }
-
-
-})(); 
-//========== /Header fixed on top animation
 
 
 
