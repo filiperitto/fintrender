c49727eecfc92e3fe61a0c83bdff598d4282c2bf
Filipe Ritto <filipe.ritto@gmail.com>
2021-05-25 10:00:33 -0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,39 @@
+(function () {
 
+    var headerScroll = getId('header-main'),
+        scrollHeight = 10,
+        classHeader = 'active';
+
+    //SCROLL
+    window.addEventListener("scroll", scrollOn);
+
+    function scrollOn() {
+        animatedScroll(headerScroll, classHeader, scrollHeight);
+    }
+
+    //Função que on scroll muda o comportamento do elemento
+    function animatedScroll(element, classN, height) {
+        y = pageYOffset; //resgata do objeto window o valor pageYOffset e guarda na variável
+        if (y > height) {
+            element.className = classN;
+        } else {
+            element.className = '';
+        }
+    }
+
+    // Função toggle adiciona ou tira a class do elemento
+    function toggle(element, classe) {
+        element.className = element.className ? '' : classe;
+    }
+
+    //Função que retorna o id do elemento
+    function getId(id) {
+        return document.getElementById(id);
+    }
+
+
+})();
+//------------------------ Header
 
 //-== Header fixed on top animation
 (function () {
